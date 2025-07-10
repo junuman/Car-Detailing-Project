4b08a3b161e545878aaf282ba1db932fbe285099
junuman <junnu.mohammed@gmail.com>
2025-07-10 19:38:15 -0400

    Proper Meta Pixel setup and cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-// App.js
+// src/App.js
 import React, { useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
@@ -26,38 +26,43 @@ function App() {
     return () => sections.forEach(section => observer.unobserve(section));
   }, []);
 
-  // Meta Pixel script injection
+  // Meta Pixel initialization
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://connect.facebook.net/en_US/fbevents.js';
-    script.async = true;
-    script.onload = () => {
-      if (!window.fbq) {
-        window.fbq = function () {
-          window.fbq.callMethod
-            ? window.fbq.callMethod.apply(window.fbq, arguments)
-            : window.fbq.queue.push(arguments);
-        };
-        window.fbq.queue = [];
-        window.fbq.loaded = true;
-        window.fbq.version = '2.0';
-      }
+    if (window.fbq) return;
 
-      window.fbq('init', '1679573929421803');
-      window.fbq('track', 'PageView');
-    };
-    document.head.appendChild(script);
+    (function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod
+          ? n.callMethod.apply(n, arguments)
+          : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = true;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = true;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+    window.fbq('init', '1679573929421803');
+    window.fbq('track', 'PageView');
   }, []);
 
   return (
     <div className="App">
+      {/* Meta Pixel NoScript fallback */}
       <noscript>
         <img
           height="1"
           width="1"
           style={{ display: 'none' }}
           src="https://www.facebook.com/tr?id=1679573929421803&ev=PageView&noscript=1"
-          alt=""
+          alt="Meta Pixel"
         />
       </noscript>
 
